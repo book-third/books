42204ba9eefda64da8fdbb71a6f5feae42c8258a
beauty-danny <1634105347@qq.com>
2016-11-22 00:14:00 +0800

    '读书链接'

--- pages/reading/biji/biji.js
+++ pages/reading/biji/biji.js
@@ -1,3 +1,24 @@
 app.controller('bijiCtrl',['$scope',function($scope){
 	$scope.a='sss';
 }])
+//弹出
+app.directive('zhezhao',[
+	function(){
+		return{
+			restrict:'A',
+			replace:true,			
+			template:'<div>·查看全部·</div>',
+			link:function($scope,el){
+				$(el).on('click',function(){
+					console.log($('#box .zz'))
+						$('#box .zz').css('display','block');
+//						return false;
+					})
+//					$('.box .zz').on('click',false);
+					$('#box .zz').on('click',function(){
+						$('#box .zz').css('display','none');
+					})
+			}
+		}
+	}
+])
